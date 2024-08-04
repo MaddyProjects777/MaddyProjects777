1c9e86375329438e0c89af9e2ed2f0c24999ca7a
MADHUKIRAN A.E <madhukiranae777@gmail.com>
2024-08-04 23:24:35 +0530

    Add files via upload

---
+++ Print Modifiers.py
@@ -0,0 +1,6 @@
+print('"Hi Everyone!", My Name Is Madhukiran A.E, ')
+#Method 1 _ using of double qote in string by using single and double qote.
+print("Hey Hi!: \"Hopefully\" Everyone Doing Good")
+#Method 2 _ using of slash to differentiate.
+print("QUOTE : \"GOOD DAY\" = \"GRT DAY\"")
+print("i am good ! : \"happy day\":\"excellent day\":\"wonderful day\":\"bright day\":\"crazy day\"")
---
+++ print basic.py
@@ -0,0 +1,5 @@
+print("My name is MADHUKIRAN A.E")
+print("Age :", "21")
+print("student @ BMSIT")
+print("madhukiranae777@gmail.com")
+print("LINKEDIN :","MADHUKIRAN A.E")
